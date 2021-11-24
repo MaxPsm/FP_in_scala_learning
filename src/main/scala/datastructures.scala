4bb9a6355009b194634ee6995e3d9239f2dab3aa
maxpsm <maxx11432@gmail.com>
2021-11-24 18:27:42 +0300

    add tasks until 3.22

--- src/main/scala/datastructures.scala
+++ src/main/scala/datastructures.scala
@@ -127,11 +127,80 @@ object datastructures {
       def go:BtoB = foldRight(as, innerIdent)(combinerDelayer)
       go(outerIdent)
     }
+
+    def appendViaFoldRight[A](l: List[A], r: List[A]): List[A] = {
+      foldRight(l, r)(Cons(_, _))
+    }
+
+    def concat[A](l: List[List[A]]): List[A] = {
+      foldRight(l, Nil: List[A])(append)
+    }
+
+    def add1(l: List[Int]): List[Int] = {
+      foldRight(l, Nil: List[Int])((a, b) => Cons(a + 1, b))
+    }
+
+    def doubleToString(l: List[Double]): List[String] = {
+      foldRight(l, Nil: List[String])((h, t) => Cons(h.toString, t))
+    }
+
+    def map[A,B](l: List[A])(f: A => B): List[B] =
+      foldRight(l, Nil:List[B])((h,t) => Cons(f(h),t))
+
+    def map_1[A,B](l: List[A])(f: A => B): List[B] =
+      foldRightViaFoldLeft(l, Nil:List[B])((h,t) => Cons(f(h),t))
+
+    def map_2[A,B](l: List[A])(f: A => B): List[B] = {
+      val buf = new collection.mutable.ListBuffer[B]
+      def go(l: List[A]): Unit = l match {
+        case Nil => ()
+        case Cons(h,t) => buf += f(h); go(t)
+      }
+      go(l)
+      List(buf.toList: _*) // converting from the standard Scala list to the list we've defined here
+    }
+
+    def filter[A](as: List[A])(f: A => Boolean): List[A] = {
+      foldRight(as, Nil: List[A])((h, t) => if (f(h)) Cons(h, t) else t)
+    }
+
+    def filter_1[A](l: List[A])(f: A => Boolean): List[A] =
+      foldRightViaFoldLeft(l, Nil:List[A])((h,t) => if (f(h)) Cons(h,t) else t)
+
+    def filter_2[A](l: List[A])(f: A => Boolean): List[A] = {
+      val buf = new collection.mutable.ListBuffer[A]
+      def go(l: List[A]): Unit = l match {
+        case Nil => ()
+        case Cons(h,t) => if (f(h)) buf += h; go(t)
+      }
+      go(l)
+      List(buf.toList: _*) // converting from the standard Scala list to the list we've defined here
+    }
+
+    def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] = {
+      concat(map(as)(f))
+    }
+
+    def flatMap2[A,B](as: List[A])(f: A => List[B]): List[B] = {
+      foldRight(as, Nil: List[B])((x, y) => append(f(x), y))
+    }
+
+    def filterViaFlatMap[A](l: List[A])(f: A => Boolean): List[A] =
+      flatMap(l)(a => if (f(a)) List(a) else Nil)
+
+    def addPairwise(a: List[Int], b: List[Int]): List[Int] = (a,b) match {
+      case (_, Nil) => Nil
+      case (Nil, _) => Nil
+      case (Cons(h1, t1), Cons(h2, t2)) => Cons(h1 + h2, addPairwise(t1, t2))
+    }
   }
 
 
   def main(args: Array[String]): Unit = {
     import List._
+
+    val simpleList = List(1, 2, 3)
+    val simpleList2 = List(4, 5, 6, 7)
 
     val l = foldRight(List(1,2,3), Nil:List[Int])(Cons(_,_))
     println(l)
@@ -141,6 +210,19 @@ object datastructures {
 
     val l3 = foldRightViaFoldLeft_1(List(1,2,3), Nil:List[Int])((a, b) => Cons(a, b))
     println(l3)
+
+    val l4 = append(List(1, 2, 3), List(4, 5, 6))
+    println(l4)
+
+    val l5 = concat(List(List(1, 2, 3), List(4, 5, 6)))
+    println(l5)
+
+    println(add1(simpleList))
+    println(filter(simpleList)(_%2 == 0))
+    println(flatMap(List(1,2,3))(i => List(i,i)))
+    println(flatMap2(List(1,2,3))(i => List(i,i)))
+    println(filterViaFlatMap(simpleList)(_%2 == 0))
+    println(addPairwise(simpleList, simpleList2))
   }
 
 }
