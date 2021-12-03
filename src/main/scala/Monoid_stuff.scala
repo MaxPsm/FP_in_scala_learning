bd5723c755ee27559498d98b820d62de8c59fc68
maxpsm <maxx11432@gmail.com>
2021-12-03 11:07:25 +0300

    added parallelism && testing

---
+++ src/main/scala/Monoid_stuff.scala
@@ -0,0 +1,20 @@
+object Monoid_stuff {
+  trait Monoid[A] {
+    def op(a1: A, a2: A): A
+    def zero: A
+  }
+
+  object Monoid {
+    val stringMonoid = new Monoid[String] {
+      def op(a1: String, a2: String) = a1 + a2
+      val zero = ""
+    }
+
+    def listMonoid[A] = new Monoid[List[A]] {
+      def op(a1: List[A], a2: List[A]) = a1 ++ a2
+      val zero = Nil
+    }
+
+  }
+
+}
--- src/main/scala/testing.scala
+++ src/main/scala/testing.scala
@@ -1,14 +1,58 @@
 import pure_functional_state.{RNG, State}
+import testing.Prop.{Falsified, MaxSize, Passed, Result, TestCases}
+
+
 
 object testing {
-  trait Prop {
-    def check: Boolean
-    def &&(p: Prop): Prop = new Prop {
-      def check: Boolean = Prop.this.check && p.check
+  case class Prop(run: (MaxSize,TestCases,RNG) => Result) {
+    def &&(p: Prop): Prop = Prop {
+      (max, n, rng) =>
+        run(max, n, rng) match {
+          case Passed => p.run(max, n, rng)
+          case x => x
+        }
     }
+
+    def ||(p: Prop): Prop = Prop {
+      (max, n, rng) =>
+        run(max, n, rng) match {
+          case Falsified(msg, _) => p.tag(msg).run(max,n,rng)
+          case x => x
+        }
+    }
+
+    def tag(msg: String) = Prop {
+      (max,n,rng) => run(max,n,rng) match {
+        case Falsified(e, c) => Falsified(msg + "\n" + e, c)
+        case x => x
+      }
+    }
+
   }
 
-  case class Gen[A](sample: State[RNG,A]) {
+  object Prop {
+    type SuccessCount = Int
+    type TestCases = Int
+    type MaxSize = Int
+    type FailedCase = String
+
+    sealed trait Result {
+      def isFalsified: Boolean
+    }
+    case object Passed extends Result {
+      def isFalsified = false
+    }
+    case class Falsified(failure: FailedCase,
+                         successes: SuccessCount) extends Result {
+      def isFalsified = true
+    }
+
+  }
+
+  case class Gen[+A](sample: State[RNG,A]) {
+    def map[B](f: A => B): Gen[B] =
+      Gen(sample.map(f))
+
     def boolean: Gen[Boolean] =
       Gen(State(RNG.boolean))
 
@@ -20,6 +64,15 @@ object testing {
 
     def listOfN(size: Gen[Int]): Gen[List[A]] =
       size flatMap (n => this.listOfN(n))
+
+    def unsized: SGen[A] = SGen(_ => this)
+
+
+    def map2[B,C](g: Gen[B])(f: (A,B) => C): Gen[C] =
+      Gen(sample.map2(g.sample)(f))
+
+    def **[B](g: Gen[B]): Gen[(A,B)] =
+      (this map2 g)((_,_))
   }
 
   object Gen {
@@ -48,5 +101,47 @@ object testing {
 
       Gen(State(RNG.double).flatMap(d => if (d < g1Threshold) g1._1.sample else g2._1.sample))
     }
+
+    def listOf1[A](g: Gen[A]): SGen[List[A]] =
+      SGen(n => g.listOfN(n max 1))
   }
+
+  object ** {
+    def unapply[A,B](p: (A,B)) = Some(p)
+  }
+
+  case class SGen[+A](g: Int => Gen[A]) {
+    def apply(n: Int): Gen[A] = g(n)
+
+    def map[B](f: A => B): SGen[B] =
+      SGen{g(_) map f }
+
+    def flatMap[B](f: A => SGen[B]): SGen[B] = {
+      val g2: Int => Gen[B] = n => {
+        g(n) flatMap { f(_).g(n) }
+      }
+      SGen(g2)
+    }
+
+    def **[B](s2: SGen[B]): SGen[(A,B)] =
+      SGen(n => apply(n) ** s2(n))
+
+  }
+
+
+  def main(args: Array[String]): Unit = {
+
+
+    val ns = List[Int](4, 15, 6)
+    val nss = ns.sorted
+    val z = nss.zip(nss.tail)
+
+    // We specify that every sorted list is either empty, has one element,
+    // or has no two consecutive elements `(a,b)` such that `a` is greater than `b`.
+    val a = nss.isEmpty || nss.tail.isEmpty || !nss.zip(nss.tail).exists {
+      case (a,b) => a > b
+    }
+  println(z)
+  }
+
 }
